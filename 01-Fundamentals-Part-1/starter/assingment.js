d1d2ffd420458e1e4b202b8f5d6bf5e7266f74bb
daem01 <140919853+daem01@users.noreply.github.com>
2024-03-02 15:19:07 -0600

    030224

---
+++ 01-Fundamentals-Part-1/starter/assingment.js
@@ -0,0 +1,5 @@
+let country = 'Switzerland';
+let continent = 'Europe';
+let population = 8700000;
+
+console.log(country, 'is located in', continent, 'and has a population of:', population);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,72 @@
+/* Variables
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = 'Bob';
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable name conventions
+let frank_bob = 'FB';
+let $function = 1;
+
+let person = 'Bob';
+let PI = 3.1415;
+
+let myCurentJob = 'IT Support Specialist';
+let myNextJob = 'Front End Developer';
+// VS.
+let job1 = 'IT Support Specialist';
+let job2 = 'Font End Developer';
+
+console.log(myCurentJob);
+*/
+
+/* Data Types
+let javaScriptIsFun = true;
+console.log('JavaScript is FUN!', javaScriptIsFun);
+
+console.log('true is a:', typeof true);
+console.log('javaScriptIsFun is defined "true":', typeof javaScriptIsFun);
+console.log('The number 23:', typeof 23);
+console.log("'Bob':", typeof 'Bob');
+
+javaScriptIsFun = 'YES!';
+console.log("javaScriptIsFun redefined 'YES!':", javaScriptIsFun);
+
+let year;
+console.log('Variable year left undefined:', year);
+console.log('Type of undefined variable:', typeof year);
+
+year = 1991;
+console.log('Variable year defined:', year);
+
+console.log('typeof null:', typeof null);
+*/
+
+/* let, const, var
+let age = 30;
+age = 31; // Reassingment or Mutation
+
+const birthYear = 1991;
+// birthYear = 1990; Throws TypeError
+
+// const job; Throws SyntaxError - Needs initial value
+
+// BEST CODING PRACTICE IS ALWAYS USE CONST UNLESS NEEDED TO CHANGE VARIABLE VALUE
+// AVOID var
+
+var job = 'Programmer';
+job = 'Support Specialist';
+
+// No Declaration still works but terrible idea
+lastName = 'Moser';
+console.log(lastName);
+*/
+
+// Basic Operators
