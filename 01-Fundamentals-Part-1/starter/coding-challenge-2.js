4b71fcefc8b7e2fc58fbd2e72b041b3beb31c49a
daem01 <140919853+daem01@users.noreply.github.com>
2024-03-08 21:13:18 -0600

    030824

---
+++ 01-Fundamentals-Part-1/starter/coding-challenge-2.js
@@ -0,0 +1,68 @@
+// Masses
+let massMark = 78;
+let massJohn = 92;
+
+// Heights
+let heightMark = 1.69;
+let heightJohn = 1.95;
+
+// Units of Measure
+const weightUnit = 'kg';
+const heightUnit = 'm';
+
+// BMIs
+let BMIMark = massMark / heightMark ** 2;
+let BMIJohn = massJohn / heightJohn ** 2;
+
+console.log("Data 1:");
+console.log("Mark weighs", massMark + weightUnit, "and is", heightMark + heightUnit, "tall.")
+console.log("Mark's BMI is", BMIMark);
+
+console.log("John weighs", massJohn + weightUnit, "and is", heightJohn + heightUnit, "tall.")
+console.log("John's BMI is", BMIJohn);
+
+console.log("Is Mark's BMI heigher than John's:", BMIMark > BMIJohn);
+
+if (BMIMark > BMIJohn) {
+    console.log(`Mark's BMI is higher than John's!`);
+} else {
+    console.log(`John's BMI is higher than Mark's!`);
+};
+
+if (BMIMark > BMIJohn) {
+    console.log(`Mark's BMI ${BMIMark} is higher than John's ${BMIJohn}!`);
+} else {
+    console.log(`John's BMI ${BMIJohn} is higher than Mark's ${BMIMark}!`);
+};
+
+console.log(" ");
+
+massMark = 95;
+massJohn = 85;
+
+heightMark = 1.88;
+heightJohn = 1.76;
+
+BMIMark = massMark / heightMark ** 2;
+BMIJohn = massJohn / heightJohn ** 2;
+
+console.log("Data 2:");
+console.log("Mark weighs", massMark + weightUnit, "and is", heightMark + heightUnit, "tall.")
+console.log("Mark's BMI is", BMIMark);
+
+console.log("John weighs", massJohn + weightUnit, "and is", heightJohn + heightUnit, "tall.")
+console.log("John's BMI is", BMIJohn);
+
+console.log("Is Mark's BMI heigher than John's:", BMIMark > BMIJohn);
+
+if (BMIMark > BMIJohn) {
+    console.log(`Mark's BMI is higher than John's!`);
+} else {
+    console.log(`John's BMI is higher than Mark's!`);
+};
+
+if (BMIMark > BMIJohn) {
+    console.log(`Mark's BMI ${BMIMark} is higher than John's ${BMIJohn}!`);
+} else {
+    console.log(`John's BMI ${BMIJohn} is higher than Mark's ${BMIMark}!`);
+};
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -149,3 +149,12 @@ if (age >= 18) {
     const yearsLeft = 18 - age;
     console.log(`Sarah cannot start driving. 😒 Wait another ${yearsLeft} years.`);
 }
+
+const birthYear = 2001;
+let century;
+if (birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+};
+console.log(`${century}st Century`);
