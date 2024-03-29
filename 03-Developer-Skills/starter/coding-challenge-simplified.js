c8c103f9dafd6b6a9a1dd5b304eae120c8b829c6
daem <douglas.ae.moser@gmail.com>
2024-03-29 16:11:26 -0500

    032924

--- 03-Developer-Skills/starter/coding-challenge-simplified.js
+++ 03-Developer-Skills/starter/coding-challenge-simplified.js
@@ -28,7 +28,7 @@ const testData2 = [12, 5, -5, 0, 4];
 const printForecast = (arr) => {
     let string = '';
     for (let i = 0; i < arr.length; i++) {
-        string += `... ${arr[i]}°F in ${i + 1} days`
+        string += `... ${arr[i]}°F in ${i + 1} days `
     }
     console.log(string);
 }
