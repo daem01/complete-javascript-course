54c34d9d5cd59ad4e2676d935142c6eadadefb32
daem01 <douglas.ae.moser@gmail.com>
2024-03-30 22:48:15 -0500

    033024

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -56,15 +56,4 @@ document.querySelector('.check').addEventListener('click', () => {
   }
 });
 
-/* CODING CHALLENGE */
-document.querySelector('.again').addEventListener('click', () => {
-  score = 20;
-  secretNumber = Math.trunc(Math.random() * 20) + 1;
-
-  document.querySelector('.score').textContent = score;
-  document.querySelector('.number').textContent = '?';
-  document.querySelector('.number').style.width = '15rem';
-  document.querySelector('.message').textContent = 'Start guessing...';
-  document.querySelector('.guess').value = '';
-  document.querySelector('body').style.backgroundColor = '#222';
-});
+/* CODING CHALLENGE.js */
