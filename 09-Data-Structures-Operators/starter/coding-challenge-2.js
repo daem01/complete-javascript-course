ef3baa23f97ee00b31bc999cb4e2ade2865b5d40
Doug <140919853+daem01@users.noreply.github.com>
2024-05-06 14:56:36 -0500

    050624

--- 09-Data-Structures-Operators/starter/coding-challenge-2.js
+++ 09-Data-Structures-Operators/starter/coding-challenge-2.js
@@ -39,6 +39,17 @@ const game = {
   },
 };
 
-for (const player of Object.entries(game.scored)) {
-  console.log(`Goal`);
+for (const [goal, player] of Object.entries(game.scored)) {
+  console.log(`Goal ${Number(goal) + 1}: ${player}`);
 }
+
+// const test = Object.entries(game.odds);
+// console.log(test);
+
+// let sumOfOdds = 0;
+// const { team1, x: draw, team2 } = { ...game.odds };
+
+// for () {
+//   sumOfOdds = team1 + draw + team2;
+// }
+// console.log(sumOfOdds);
