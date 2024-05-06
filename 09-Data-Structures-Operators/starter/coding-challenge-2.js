7222ada11ce786b76dca5ba0b84595c184883287
daem01 <140919853+daem01@users.noreply.github.com>
2024-05-06 18:37:40 -0500

    050624

--- 09-Data-Structures-Operators/starter/coding-challenge-2.js
+++ 09-Data-Structures-Operators/starter/coding-challenge-2.js
@@ -39,17 +39,28 @@ const game = {
   },
 };
 
+// 1 -- Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
 for (const [goal, player] of Object.entries(game.scored)) {
   console.log(`Goal ${Number(goal) + 1}: ${player}`);
 }
 
-// const test = Object.entries(game.odds);
-// console.log(test);
+// 2 -- Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+let sum = 0;
+for (const [key, value] of Object.entries(game.odds)) {
+  sum += value;
+}
 
-// let sumOfOdds = 0;
-// const { team1, x: draw, team2 } = { ...game.odds };
+let avg = sum / 3;
+avg = avg.toFixed(2);
+console.log(`Average odds: ${avg}`);
 
-// for () {
-//   sumOfOdds = team1 + draw + team2;
-// }
-// console.log(sumOfOdds);
+// 3 -- Print the 3 odds to the console, but in a nice formatted way
+const { team1: team1Odds, x: drawOdds, team2: team2Odds } = game.odds;
+
+console.log(`Odds of ${game.team1} winning: ${team1Odds}`);
+console.log(`Odds of draw: ${drawOdds}`);
+console.log(`Odds of ${game.team2} winning: ${team2Odds}`);
+
+// 4 -- Bonus: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value.
+const scorers = { ...game.scored };
+console.log(scorers);
