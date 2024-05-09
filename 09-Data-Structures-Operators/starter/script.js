91d9476df5f09f49fdc7a71460721902d30b4a8e
Doug <140919853+daem01@users.noreply.github.com>
2024-05-09 16:38:37 -0500

    050924

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -32,7 +32,7 @@ const restaurant = {
   openingHours,
 
   // ES6 enhanced methods | no function call
-  order(starterIndex, mainIndex) {
+  order /*: function*/(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
@@ -361,7 +361,7 @@ const entries = Object.entries(openingHours);
 // Looping through object with destructuring
 for (const [day, { open, close }] of entries) {
   console.log(`On ${day} we open at ${open} and close at ${close}`);
-} */
+} 
 
 /////////////////////////
 // SETS
@@ -396,4 +396,80 @@ const staffUnique = [...new Set(staff)];
 console.log(staffUnique);
 console.log(new Set(staff).size);
 
-console.log(new Set('douglasmoser').size);
+console.log(new Set('douglasmoser').size); 
+
+//////////////////////////
+// Maps: Fundamentals
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon Portugal'));
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open')
+  .set(false, 'We are closed');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+console.log(rest);
+console.log(rest.size);
+rest.clear();
+console.log(rest, rest.size);
+
+rest.set([1, 2], 'Test');
+console.log(rest);
+console.log(rest.get([1, 2])); // undefined
+const arr = [1, 2];
+rest.set(arr, 'Test 2');
+console.log(rest.get(arr)); // Returns
+
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest); 
+
+////////////////////////
+// Maps: Iteration
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct 🎉'],
+  [false, 'Try again! 😢'],
+]);
+console.log(question);
+
+// Convert Object to Map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+// Quiz App
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+// const answer = Number(prompt('Your answer:'));
+const answer = 3;
+console.log(answer);
+
+console.log(question.get(question.get('correct') === answer)); // Returns true or false
+
+// Convert Map to Array
+console.log([...question]);
+// console.log([...question.entries()]);
+console.log([...question.keys()]);
+console.log([...question.values()]); */
+
+/////////////////////////////////////////
+// Summary: Which Data Sturcture To Use?
