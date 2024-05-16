550b87152c92ea3d5262babfcdf6a4818f5db1aa
Doug <140919853+daem01@users.noreply.github.com>
2024-05-16 15:02:21 -0500

    051624

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -164,7 +164,53 @@ book.apply(swiss, flightData);
 console.log(swiss);
 
 // Modern Method -- same as apply method
-book.call(swiss, ...flightData); */
+book.call(swiss, ...flightData);
 
 ///////////////////
-// The Bind Method
+// The Bind Method (if uncommenting include previous lesson code)
+// book.call(eurowings, 23, 'Sarah Williams');
+
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+
+bookEW(23, 'Steven Williams');
+
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Doug Allen');
+bookEW23('Martha Cooper');
+
+// With Event Listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this); // Points to element rather than object
+
+  this.planes++;
+  console.log(this.planes);
+};
+// lufthansa.buyPlane();
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa)); // need to define 'this' keyword with bind method
+
+// Partial application
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23); // 'null' is customary for setting 'this' keyword when there is no 'this' keyword in function
+// addVAT = value => value + value * 0.23; is what this is doing
+
+console.log(addVAT(100));
+console.log(addVAT(23));
+
+// Same thing but using function that returns another function that we learned about earlier
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
+console.log(addVAT2(23)); */
