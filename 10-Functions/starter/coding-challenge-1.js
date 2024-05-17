1d7fd42d20ed62948ba6a05910c00aa2b77752a8
daem01 <140919853+daem01@users.noreply.github.com>
2024-05-16 19:06:23 -0500

    051624

--- 10-Functions/starter/coding-challenge-1.js
+++ 10-Functions/starter/coding-challenge-1.js
@@ -10,10 +10,39 @@ poll.registerNewAnswer = function () {
     `What is your favourite programming language? \n${this.options[0]}\n${this.options[1]}\n${this.options[2]}\n${this.options[3]}`
   );
 
-  if (ans === 0) this.answers[0]++;
-  console.log(this.answers);
+  switch (ans) {
+    case '0':
+      this.answers[0]++;
+      break;
+
+    case '1':
+      this.answers[1]++;
+      break;
+
+    case '2':
+      this.answers[2]++;
+      break;
+
+    case '3':
+      this.answers[3]++;
+      break;
+
+    default:
+      alert(`${ans} is not a valid answer!`);
+      break;
+  }
+
+  this.displayResults(this.answers);
+};
+
+poll.displayResults = type => {
+  console.log(`Results...
+  Javascript: ${type[0]}
+  Python: ${type[1]}
+  Rust: ${type[2]}
+  C++: ${type[3]}`);
 };
 
 document
   .querySelector('.poll')
-  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+  .addEventListener('click', poll.registerNewAnswer.bind(poll, poll.options));
