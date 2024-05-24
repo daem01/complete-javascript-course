cb7bba36f70029421230320e3e2c6b25264713eb
daem01 <140919853+daem01@users.noreply.github.com>
2024-05-23 20:08:29 -0500

    052324

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -195,6 +195,34 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
+/////////////////////////////////////////
+// The findIndex Method // Close Account
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+    console.log(index);
+
+    // .indexOf(23)
+    // Delete account
+    accounts.splice(index, 1);
+
+    // Hide UI
+    containerApp.style.opacity = 0;
+  }
+
+  inputCloseUsername.value = inputClosePin.value = '';
+});
+
+//////////////////
+// Some and Every
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -424,6 +452,3 @@ for (const acc of accounts) {
   if (acc.owner === 'Jessica Davis') accountOf = acc;
 }
 console.log(accountOf); */
-
-////////////////////////
-// The findIndex Method
