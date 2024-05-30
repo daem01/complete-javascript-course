1d5541e73f96c9d44e929cf7fa3e8217196cb02d
Doug <140919853+daem01@users.noreply.github.com>
2024-05-30 14:33:25 -0500

    053024

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -564,3 +564,40 @@ console.log(movements); */
 
 ////////////////////////////////////////////
 // More Ways of Creating and Filling Arrays
+const arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+// Empty arrays + fill method
+const x = new Array(7);
+console.log(x);
+console.log(x.map(() => 5));
+
+x.fill(1); // mutates array
+x.fill(1, 3, 5);
+console.log(x);
+
+arr.fill(23, 4, 6);
+console.log(arr);
+
+// Array.from
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+
+const z = Array.from(
+  { length: 7 },
+  (currentValue /* _ */, currentIndex) => currentIndex + 1
+);
+console.log(z);
+
+const movementsUI = Array.from(document.querySelectorAll('.movements__value'));
+
+labelBalance.addEventListener('click', function () {
+  const movementsUI = Array.from(
+    document.querySelectorAll('.movements__value'),
+    el => el.textContent.replace('€', '')
+  );
+
+  console.log(movementsUI);
+
+  const movementsUI2 = [...document.querySelectorAll('.movements__value')];
+});
