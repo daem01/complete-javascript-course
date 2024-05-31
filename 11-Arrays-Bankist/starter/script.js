ca989b575e295ce9feeecf74e1acc59012be77b9
Doug <140919853+daem01@users.noreply.github.com>
2024-05-31 08:58:37 -0500

    053124

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -604,3 +604,31 @@ labelBalance.addEventListener('click', function () {
 
 //////////////////////////
 // Array Methods Practice
+
+// 1.
+// const bankDepositSum = accounts.map(acc => acc.movements).flat();
+const bankDepositSum = accounts
+  .flatMap(acc => acc.movements)
+  .filter(mov => mov > 0)
+  .reduce((sum, cur) => sum + cur, 0);
+
+console.log(bankDepositSum);
+
+// 2. count deposits with at least 1000 dollars
+// const numDeposits1000 = accounts
+//   .flatMap(acc => acc.movements)
+//   .filter(mov => mov >= 1000).length;
+
+const numDeposits1000 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((count, cur) => (cur >= 1000 ? ++count : count), 0);
+
+console.log(numDeposits1000);
+
+// how ++ operator works
+let a = 10;
+console.log(a++);
+console.log(a);
+console.log(++a);
+
+// 3. more advanced reduce method
