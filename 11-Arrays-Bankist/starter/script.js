8b21129563612204987f1fb3f3f9bf1906a15f5e
Doug <140919853+daem01@users.noreply.github.com>
2024-05-31 10:59:15 -0500

    053124

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -600,7 +600,7 @@ labelBalance.addEventListener('click', function () {
   console.log(movementsUI);
 
   // const movementsUI2 = [...document.querySelectorAll('.movements__value')];
-}); */
+}); 
 
 //////////////////////////
 // Array Methods Practice
@@ -632,3 +632,34 @@ console.log(a);
 console.log(++a);
 
 // 3. more advanced reduce method
+const { deposits, withdrawals } = accounts
+  .flatMap(acc => acc.movements)
+  .reduce(
+    (sums, cur) => {
+      // cur > 0 ? (sums.deposits += cur) : (sums.withdrawals += cur);
+      sums[cur > 0 ? 'deposits' : 'withdrawals'] += cur;
+
+      return sums;
+    },
+    { deposits: 0, withdrawals: 0 }
+  );
+
+console.log(deposits, withdrawals);
+
+// 4. convert any string to title case
+// this is a nice title -> This Is a Nice Title
+const convertTitleCase = function (title) {
+  const capitalize = str => str[0].toUpperCase() + str.slice(1);
+
+  const exceptions = ['a', 'an', 'and', 'the', 'but', 'or', 'on', 'in', 'with'];
+
+  const titleCase = title
+    .toLowerCase()
+    .split(' ')
+    .map(word => (exceptions.includes(word) ? word : capitalize(word)))
+    .join(' ');
+  return capitalize(titleCase);
+};
+console.log(convertTitleCase('this is a nice title'));
+console.log(convertTitleCase('this is a LONG title but not too long'));
+console.log(convertTitleCase('and here is another title with an EXAMPLE')); */
