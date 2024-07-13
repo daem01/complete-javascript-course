184494794b3ac93b0f2fd60a985bf7d4a45f797d
daem01 <140919853+daem01@users.noreply.github.com>
2024-07-12 20:50:17 -0500

    071224

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -76,4 +76,53 @@ document
   });
 
 //////////////////////////////////
-// Styles, Attributes and Classes */
+// Styles, Attributes and Classes
+// Styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.color); // we can't get styles from in a class, only what we set in JS
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.getAttribute('src'));
+console.log(logo.className);
+
+logo.alt = 'Beautiful minimalist logo';
+
+// Non-standard
+console.log(logo.designer);
+console.log(logo.getAttribute('designer'));
+logo.setAttribute('company', 'Bankist');
+
+logo.getAttribute('src');
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+// Data attributes
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add('c', 'j');
+logo.classList.remove('c', 'j');
+logo.classList.toggle('c', 'j');
+logo.classList.contains('c', 'j'); // not 'includes' like arrays
+
+// Don't use; Overrides all existing classes and only allows one class to be assigned to element
+// logo.className = 'doug'; 
+
+///////////////////////////////*/
+// Implementing Smooth Scrolling
