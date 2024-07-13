101b1c4aa4a2a6ef259d2e443cffcd19d432473e
daem01 <140919853+daem01@users.noreply.github.com>
2024-07-12 20:23:44 -0500

    071224

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -33,16 +33,47 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-//////////////////////////////////////////////
+/*////////////////////////////////////////////
 // Selecting, Creating, and Deleting Elements
 console.log(document.documentElement);
 console.log(document.head);
 console.log(document.body);
 
-document.querySelector('.header');
+const header = document.querySelector('.header');
 const allSections = document.querySelectorAll('.section');
 console.log(allSections);
 
 document.getElementById('section--1');
 const allButtons = document.getElementsByTagName('button'); // returns HTMLCollection not nodeList
 console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn')); // also returns HTMLCollection
+
+// Creating and inserting elements
+// .insertAdjacentHTML
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent =
+//   'We use cookies for improved functionality and analytics.';
+message.innerHTML =
+  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true));
+
+// header.before(message);
+// header.after(message);
+
+// Delete elements
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove(); // new
+    // Older way of doing it
+    // message.parentElement.removeChild(message);
+  });
+
+//////////////////////////////////
+// Styles, Attributes and Classes */
