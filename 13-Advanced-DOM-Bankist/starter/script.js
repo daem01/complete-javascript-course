aa1b9cfaf8e66d2e4cd7ae10fca10646a1fb476f
daem01 <140919853+daem01@users.noreply.github.com>
2024-07-14 18:42:19 -0500

    071424

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -124,5 +124,39 @@ logo.classList.contains('c', 'j'); // not 'includes' like arrays
 // Don't use; Overrides all existing classes and only allows one class to be assigned to element
 // logo.className = 'doug'; 
 
-///////////////////////////////*/
+/////////////////////////////////
 // Implementing Smooth Scrolling
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current Scroll (X/Y)', window.scrollX, window.scrollY);
+
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  // Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.scrollX,
+  //   s1coords.top + window.scrollY
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.scrollX,
+  //   top: s1coords.top + window.scrollY,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+////////////////////////////////////*/
+// Types of Events and Event Handlers
