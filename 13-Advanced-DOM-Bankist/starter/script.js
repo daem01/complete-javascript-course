943c5069986edf6ae0144d006d21ee398c96319b
daem01 <140919853+daem01@users.noreply.github.com>
2024-07-14 18:55:17 -0500

    071424

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -158,5 +158,24 @@ btnScrollTo.addEventListener('click', function (e) {
   section1.scrollIntoView({ behavior: 'smooth' });
 });
 
-////////////////////////////////////*/
+//////////////////////////////////////
 // Types of Events and Event Handlers
+const h1 = document.querySelector('h1');
+
+const alertH1 = function (e) {
+  alert('addEventListener: Great! You are reading the heading.');
+};
+
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => {
+  h1.removeEventListener('mouseenter', alertH1);
+}, 3000);
+
+// Old school way of adding event listeners || also can't remove event handler
+// h1.onmouseenter = function (e) {
+//   alert('addEventListener: Great! You are reading the heading.');
+// };
+
+///////////////////////////////////////////*/
+// Event Propagation: Bubbling and Capturing
