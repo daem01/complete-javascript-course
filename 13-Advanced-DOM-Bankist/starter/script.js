b1c42632f9eee22305561237327c280186ab990f
daem01 <140919853+daem01@users.noreply.github.com>
2024-07-14 22:09:53 -0500

    071424

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -177,5 +177,38 @@ setTimeout(() => {
 //   alert('addEventListener: Great! You are reading the heading.');
 // };
 
-///////////////////////////////////////////*/
+/////////////////////////////////////////////
 // Event Propagation: Bubbling and Capturing
+// rgb(0, 0, 0)
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+
+  // Stop propagation
+  // e.stopPropagation();
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('CONTAINER', e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+});
+
+document.querySelector('.nav').addEventListener(
+  'click',
+  function (e) {
+    this.style.backgroundColor = randomColor();
+    console.log('NAV', e.target, e.currentTarget);
+    console.log(e.currentTarget === this);
+  }
+  // , true // capturing allows parent to handle event first aka "Capturing"
+);
+
+////////////////////////////////////////////////*/
+// Event Delegation: Implementing Page Navigation
