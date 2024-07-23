4c9a6ac31de52eeeffe5ba65a76db7527af3ad04
Doug <140919853+daem01@users.noreply.github.com>
2024-07-23 16:42:02 -0500

    072324

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -341,5 +341,38 @@ window.addEventListener('scroll', function (e) {
   else nav.classList.remove('sticky');
 });
 
-///////////////////////////////*/
+/////////////////////////////////
 // The Intersection Observer API
+// const obsCallback = function (entries, observer) {
+//   entries.forEach(entry => console.log(entry));
+// }; // entries are an array of threshold entries
+
+// const obsOptions = {
+//   root: null,
+//   threshold: [0, 0.2],
+// };
+
+// const observer = new IntersectionObserver(obsCallback, obsOptions);
+// observer.observe(section1);
+
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+console.log(navHeight);
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  // console.log(entry);
+
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+headerObserver.observe(header);
+
+//////////////////////////////*/
+// Revealing Elements on Scroll
