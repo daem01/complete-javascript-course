9692297ec33fd6697370830c2b00c752ead24a2e
daem01 <140919853+daem01@users.noreply.github.com>
2024-08-11 17:41:01 -0500

    081124

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -2,6 +2,7 @@
 
 //////////////////////////////////////////////
 // Constructor Functions and the new Operator
+/*
 const Person = function (firstName, birthYear) {
   // Instance properties
   this.firstName = firstName;
@@ -82,3 +83,111 @@ const h1 = document.querySelector('h1');
 // console.log(h1.__proto__.__proto__.__proto__.__proto__.__proto__.__proto__);
 
 console.dir(x => x + 1);
+
+////////////////////////////////////////////*/
+// ES6 Classes
+// classes are still a type of function
+// 1. Classes are NOT hoisted (hoisted = can be used before decalred in the code a.k.a. classes can not be used before they are declared)
+// 2. Classes are first-class citizens (can be passed into functions, and can return from functions)
+// 3. The body of a Class is always executed in strict mode whether the file is or not
+
+// class expression
+// const PersonCl = class {};
+
+// class declaration
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  // Methods will be added ot .prototype property
+  calcAge() {
+    console.log(2024 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.fullName}!`);
+  }
+
+  // Getter/Setter Lecture
+  get age() {
+    return 2024 - this.birthYear;
+  }
+
+  // Set a property that already exists
+  set fullName(name) {
+    console.log(name);
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name.`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+
+  // Static Methods Lecture
+  static hey() {
+    console.log('Hey There Class 👋');
+    console.log(this);
+  }
+}
+
+const jessica = new PersonCl('Jessica Davis', 1996);
+console.log(jessica);
+jessica.calcAge();
+// Getter/Setter Lecture
+
+console.log(jessica.__proto__);
+
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey ${this.fullName}!`);
+// };
+jessica.greet();
+
+// const walter = new PersonCl('Walter', 1965);
+const walterWhite = new PersonCl('Walter White', 1965);
+
+//////////////////////////////////////////////
+// Setters and Getters
+const account = {
+  owner: 'doug',
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movements);
+
+console.log(jessica.age);
+
+//////////////////////////////////////////////
+// Static Methods
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+const doug = new Person('Doug', 2001);
+console.log(doug);
+
+Person.hey = function () {
+  console.log('Hey There 👋');
+  console.log(this);
+};
+
+Person.hey();
+// doug.hey(); // can't inherit
+PersonCl.hey();
+
+//////////////////////////////////////////////
+// Object.create
