9e551fa2dfe57a9dc86e1f6fc778de8096b08190
daem01 <douglasmoservii@gmail.com>
2024-11-01 11:07:46 -0500

    API URL CHANGE

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// API URL CHANGE
+// https://countries-api-836d.onrender.com/countries/
+
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
