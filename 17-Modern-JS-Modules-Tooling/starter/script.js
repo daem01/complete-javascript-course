935283524fbb851fbfa21107b1a51d8887575d22
daem01 <140919853+daem01@users.noreply.github.com>
2024-12-19 12:50:06 -0600

    122924

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -9,7 +9,7 @@ console.log('Importing module');
 // import * as ShoppingCart from './shoppingCart.js';
 // ShoppingCart.addToCart('bread', 5);
 // console.log(ShoppingCart.totalPrice, ShoppingCart.tq);
-
+*/
 // import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
 // in practice we never mix named and default exports in the same module
 import add, { cart } from './shoppingCart.js';
@@ -18,7 +18,7 @@ add('bread', 5);
 add('apple', 3);
 
 console.log(cart);
-
+/*
 /////////////////////////////////////////////////////////////////////////////////////////////////
 // Top Level AWAIT
 // Only available in modules, but blocks exuction of entire module because it's outside of async function. Can be harmful if you are waiting on long running task
@@ -98,7 +98,8 @@ console.log(ShoppingCart2.shippingCost);
 
 ////////////////////////////////////////////////////////////////////////////////////
 // Introduction to NPM
-import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+// import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+import cloneDeep from 'lodash-es';
 
 const state = {
   cart: [
@@ -115,3 +116,30 @@ state.user.loggedIn = false;
 console.log(stateClone);
 
 console.log(stateDeepClone);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+// Configuring Babel and Polyfilling
+if (module.hot) {
+  module.hot.accept();
+}
+
+class Person {
+  greeting = 'Hey';
+  constructor(name) {
+    this.name = name;
+    console.log(`${this.greeting}, ${this.name}`);
+  }
+}
+const doug = new Person('Doug');
+
+console.log('Doug' ?? null);
+
+console.log(cart.find(el => el.quantity >= 3));
+Promise.resolve('TEST').then(x => console.log(x));
+
+import 'core-js/stable';
+// import 'core-js/stable/array/find';
+// import 'core-js/stable/promise'
+
+// Polyfilling async functions
+import 'regenerator-runtime/runtime';
